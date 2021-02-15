4568fbbd74c7c8377a49c2d40e9961c83ba1bdad
MrSpectra <sam@samslade.co.uk>
2021-02-15 16:15:04 +0000

    Controller update and delete methods

--- controllers/journey-controller.js
+++ controllers/journey-controller.js
@@ -1,11 +1,12 @@
 // What needs to happen in this file:
 //
-// + Add locales            []
 // + create journey         [X]
-// + return all journeys    []
-// + return journey by id   []
-// + update journey by id   []
-// + delete journey by id   []
+// + return journey by id   [X]
+// + update journey by id   [X]
+// + delete journey by id   [X]
+//
+// + TEST                   []
+//
 // -Sam
 
 const Journey = require('../static/js/lib/journey');
@@ -18,6 +19,24 @@ class JourneyController {
   }
 
   /**
+   * A private function used to clone the journey data
+   * that was recieved from outside of this class. This
+   * is to serialize the data before it is entered into
+   * the database.
+   *
+   * @param {Journey} journey - The journey object to be cloned
+   *
+   * @returns The cloned journey object
+   */
+  async cloneJourney(journey) {
+    let newJourney = new Journey(journey.forename, journey.surname);
+    for (let i=0; i<journey.points.length; i++) {
+      newJourney.addPoint(journey.points[i]);
+    }
+    return newJourney;
+  }
+
+  /**
    * Creates a new journey in the data base by passing Journey object
    * to the model
    *
@@ -27,10 +46,8 @@ class JourneyController {
    */
   async create(journey) {
     // Clone the journey to serialize
-    let newJourney = new Journey(journey.forename, journey.surname);
-    for (let i=0; i<journey.points.length; i++) {
-      newJourney.addPoint(journey.points[i]);
-    }
+    let newJourney = await cloneJourney(journey);
+    
 
     // Send journey to the model to create it
     let res = await this.model.create(newJourney);
@@ -51,6 +68,39 @@ class JourneyController {
     let res = await this.model.read(id);
 
     // Return the database response to the view
+    this.view(res);
+  }
+
+  /**
+   * Update a given journey
+   *
+   * @param {int} id - The journey id to update
+   * @param {Journey} journey - The data to update the journey
+   *
+   * @returns The database response
+   */
+  async update(id, journey) {
+    // Clone the journey to serialize
+    let newJourney = cloneJourney(journey);
+
+    // Send the id and journey to the model to update
+    let res = await this.model.update(id, journey);
+
+    // Return the model response
+    this.view(res);
+  }
+
+  /**
+   * Delete a given journey from a given id_points
+   *
+   * @param {int} id - The id of the journey to delete
+   *
+   * @returns the database response
+   */
+  async delete(id) {
+    let res = this.model.delete(id);
+
+    // Return the model response
     this.view(res);
   }
 
