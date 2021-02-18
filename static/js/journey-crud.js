5bf55141302bf5d424ca5d0d77bf6fd9ab0c584c
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2021-02-18 23:40:25 +0000

    Add CRUD module

--- static/js/journey-crud.js
+++ static/js/journey-crud.js
@@ -4,6 +4,45 @@
 (function() {
     'use strict';
     document.addEventListener('DOMContentLoaded', () => {
-
     });
+
+    async function _getJourneys() {
+        return await (await fetch('/journeys')).json();
+    }
+
+    async function _getJourney(id) {
+        return await (await fetch(`/journeys/${id}`)).json();
+    }
+
+    async function _sendJourney(journeyObj) {
+        fetch('/journeys', {
+            method: 'POST',
+            mode: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(journeyObj)
+        }).then((res) => {
+            alert('Journey created!');
+        }).catch((error) => {
+           alert('Journey creation failed!');
+        })
+    }
+
+    async function _updateJourney(journeyID, journeyObj) {
+        fetch(`/journeys/${journeyID}/update`, {
+            method: 'POST',
+            mode: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(journeyObj)
+        }).then((res) => {
+            alert('Journey successfully updated!');
+        }).catch((error) => {
+            alert('Journey update failed!');
+        })
+    }
 })();
