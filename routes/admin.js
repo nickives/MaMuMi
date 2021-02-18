6c44039086f264ea2cd2b575dd1699f8fb2d55e3
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2021-02-18 12:22:24 +0000

    Redirect to dashboard on submit (demonstration)

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.redirect('/admin/login');
+});
+
+router.get('/login', function(req, res, next) {
     res.render('login');
+});
+
+router.get('/dashboard', function(req, res, next) {
+    res.render('admin');
 });
 
 module.exports = router;
