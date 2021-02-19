84c23151578b2d8c42c6c5d3779d80d2da763ed9
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2021-02-19 13:30:04 +0000

    Add alternating create/list panes to dash view

--- static/js/journey-crud.js
+++ static/js/journey-crud.js
@@ -3,7 +3,26 @@
 */
 (function() {
     'use strict';
+
+    let _btnPressCount = 0;
+    let _createBtn;
+    let _listPane;
+    let _creationPane;
+    let _creationForm;
+    let _tableContainer;
+    let _journeyList;
+
     document.addEventListener('DOMContentLoaded', () => {
+        _createBtn = document.getElementById('create-btn');
+        _listPane = document.getElementById('journey-list');
+        _creationPane = document.getElementById('create-journey');
+        _creationForm = document.querySelector('#create-journey form');
+        _tableContainer = document.querySelector('.table-responsive');
+        _journeyList = document.querySelector('#journey-list tbody');
+
+        _displayJourneys();
+        _createBtn.addEventListener('click', _altPane);
+        _creationForm.addEventListener('submit', _addJourney);
     });
 
     async function _getJourneys() {
@@ -60,5 +79,31 @@
         }).catch((error) => {
             alert('Journey deletion failed!');
         })
+    }
+
+    function _displayJourneys() {
+        _getJourneys().then(journeys => {
+            if(journeys.length > 0) {  
+            } else {
+                _tableContainer.innerText = 'No journeys added yet. Let\'s get started!';
+            }
+        })
+    }
+
+    function _altPane(event) {
+        if(!(++_btnPressCount % 2 == 0)) {
+            // Odd
+            _createBtn.innerText = 'List';
+            _creationPane.classList.add('active-pane');
+            _listPane.classList.remove('active-pane');
+
+        } else {
+            // Even
+            _createBtn.innerText = 'Create';
+            _creationPane.classList.remove('active-pane');
+            _listPane.classList.add('active-pane');
+        }
+
+        event.preventDefault();
     }
 })();
