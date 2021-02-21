e3a357c79791be478aaae1113a99e954cb124fd5
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2021-02-21 22:57:44 +0000

    Set lat/lng input values on marker being set

--- static/js/journey-crud.js
+++ static/js/journey-crud.js
@@ -116,14 +116,26 @@ function initMap() {
         });
 
     map.setOptions({disableDoubleClickZoom: true });
+
     const pointMarker = new google.maps.Marker({map: map});
+    const latInput = document.getElementById('lat');
+    const lngInput = document.getElementById('lng');
 
     map.addListener('dblclick', (e) => {
         placeMarkerAndPanTo(e.latLng, map, pointMarker);
+        setLatLng(e.latLng, latInput, lngInput);
     })
 }
 
+// Set position of marker and pan map to marker location
 function placeMarkerAndPanTo(latLng, map, marker) {
     marker.setPosition(latLng);
     map.panTo(latLng);
-  }
+}
+
+// Set lat/lng fields of Point form based on marker's location
+function setLatLng(latLng, latInput, lngInput) {
+    const loc = JSON.parse(JSON.stringify(latLng.toJSON()));
+    latInput.value = loc.lat;
+    lngInput.value = loc.lng;
+}
