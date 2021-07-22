242f42c3deca55f31f937771dedd495fbc9c2404
Nicholas Ives <nickives@gmail.com>
2021-07-22 14:58:18 +0100

    This test is totally broken

--- test/journey-controller-test.js
+++ test/journey-controller-test.js
@@ -1,40 +1,46 @@
 const Point = require('../static/js/lib/point');
 const Journey = require('../static/js/lib/journey');
 const assert = require('assert');
-const pool = require('./pool-test');
 const JourneyModel = require("../models/journeys-model");
 const JourneyController = require("../controllers/journey-controller");
 const sinon = require('sinon');
 const { log } = require('console');
+const { mockReq, mockRes } = require('sinon-express-mock');
 
 const res = require('../node_modules/express/lib/response');
 
 describe('Journeys-controller.js api test', function () {
-  let journey;
-  let model = new JourneyModel(pool);
-  let mockModel = sinon.mock(model);
+  let journey, mockView, mockModel, controller;
 
-  let mockView = sinon.stub(res);
-
-  let controller = new JourneyController(model, mockView);
-
-  beforeEach(async () => {
+  beforeEach( () => {
       // test journey
-      point1 = new Point(null, 1, {lat: 51.887491030242344, lng: -2.0881395483987832}, "http://video.link", new Date(), null);
-      point2 = new Point(null, 2, {lat: 52, lng: -2}, "point2", new Date(), null);
+      point1 = new Point(null, 1, {lat: 51.887491030242344, lng: -2.0881395483987832});
+      point2 = new Point(null, 2, {lat: 52, lng: -2});
       desc = { 
           en: "En description",
           es: "Es description",
           de: "De description",
           fr: "Fr description"
       };
-      journey = new Journey("Bob", "FromTesco");
+      journey = new Journey("Bob", "FromTesco", "http://video.link");
+      journey.addDescription(desc);
       journey.addPoint(point1);
       journey.addPoint(point2);
-      point1.addDescription(desc);
-      point2.addDescription(desc);
+
+      mockModel = sinon.mock(JourneyModel);
+      mockView = mockRes();
+
+      controller = new JourneyController(mockModel, mockView);
   });
 
+  afterEach(() => {
+    // Restore the default sandbox here
+    sinon.restore();
+  });
+
+  const getController = (model) => {
+    return new JourneyController(model, mockView);
+  }
 
   it('Should create journey OK', async () => {
     expectation = mockModel.expects("create").once().withArgs(journey);
@@ -48,7 +54,7 @@ describe('Journeys-controller.js api test', function () {
     assert.ok(expectation.verify());
   });
 
-  it('Should get update journey OK',  async () => {
+  it('Should update journey OK',  async () => {
     expectation = mockModel.expects("update").once().withArgs(1,journey);
     await controller.update(1, journey);
     assert.ok(expectation.verify());
