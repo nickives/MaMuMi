01a9ec2ba3bd53325e773f637ab8cd1f7aab502a
Nicholas Ives <nickives@gmail.com>
2021-09-22 15:43:35 +0100

    Initial login support - system now logs in and redirects!

--- routes/admin.js
+++ routes/admin.js
@@ -4,27 +4,46 @@ const JourneyModel = require('../models/journeys-model');
 const SessionModel = require('../models/session-model');
 const pool = require("../lib/db/pool");
 const formidable = require('formidable');
+const CryptoJS = require('crypto-js');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.redirect('/admin/login');
+router.get('/', async function (req, res, next) {
+    let isLoggedIn = false;
+    if (req.cookies && req.cookies.sessionKey) {
+        const sessionKey = req.cookies.sessionKey;
+        const model = new SessionModel(pool);
+        isLoggedIn = await model.isSessionIdValid(sessionKey);
+    }
+
+    if (isLoggedIn) {
+        res.redirect('/admin/dashboard');
+    } else {
+        res.redirect('/admin/login');
+    }
+    
 });
 
 router.get('/login', function (req, res, next) {
     res.render('login');
 });
 
-router.post('/login', function (req, res, next) {
-    const form = formidable();
+router.post('/login', async function (req, res, next) {
 
-    form.parse(req, async function (err, fields, file, next) {
-        if (err) {
-            next(err);
-            return;
+    try {
+        const expectedPasscodeHash = process.env.APP_ADMIN_PASS_SHA256;
+        const passcodeHash = CryptoJS.SHA256(req.body.password).toString();
+
+        if (passcodeHash === expectedPasscodeHash) {
+            const model = new SessionModel(pool);
+            const sessionKey = await model.create();
+            res.cookie('sessionKey', sessionKey);
+            res.redirect('/admin/dashboard');
+        } else {
+            res.status(403).render('login', { badPassword: true } );
         }
-
-
-    });
+    } catch {
+        res.sendStatus(500);
+    }
 });
 
 router.get('/dashboard', async function (req, res) {
